a52cb307637194f20b6cc6b31830fc0f6a47565b
saadsidd007 <saadsiddiqui8532@gmail.com>
2025-09-28 03:12:16 +0530

    s

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -10,9 +10,8 @@ export default async function handler(req, res) {
     return res.status(403).json({ message: "Forbidden" });
   }
 
-  if (req.method !== "GET" && req.method !== "HEAD") {
-    res.setHeader("Allow", ["GET", "HEAD"]);
-    return res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Only GET requests allowed" });
   }
 
   try {
